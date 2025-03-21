1787789cdfd82aee8866b4d905390067c1ada294
Digitalguyco <youngkhito@gmail.com>
2025-03-21 10:56:02 +0100

    admin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,7 +76,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'communications@coinease.live'
 EMAIL_HOST_PASSWORD = 'Calebrandthemall9#'
-ADMIN_EMAIL = 'youngkhito@gmail.com'
+ADMIN_EMAIL = 'coinease7@gmail.com'
 DEFAULT_FROM_EMAIL = 'communications@coinease.live'
 SITE_URL = 'https://coinease.live'
 
