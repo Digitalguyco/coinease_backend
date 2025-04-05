c52596295722882dd6f763cf51da4e8c0f73b834
Digitalguyco <youngkhito@gmail.com>
2025-04-05 18:11:24 +0100

    fitempx

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -91,7 +91,7 @@ class SignalPlan(models.Model):
 # Signal Purchase History
 class SignalPurchaseHistory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='signal_purchases')
-    # plan = models.ForeignKey(SignalPlan, on_delete=models.SET_NULL, null=True)
+    plan = models.ForeignKey(SignalPlan, on_delete=models.SET_NULL, null=True)
     amount = models.DecimalField(max_digits=18, decimal_places=8)
     # transaction = models.ForeignKey('transactions.Transaction', on_delete=models.SET_NULL, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
